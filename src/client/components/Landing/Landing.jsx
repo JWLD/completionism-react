d545031956767d1034a2940d6306f1f4d2aeac7b
JWLD <jackwldavies@icloud.com>
2017-12-27 23:09:22 +0000

    add icons to categories on landing page

--- src/client/components/Landing/Landing.jsx
+++ src/client/components/Landing/Landing.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import categories from 'constants/categories';
+import { WOWHEAD_ICON } from 'constants/urls';
 import './Landing.scss';
 
 const Landing = () => {
 	const wraps = Object.keys(categories).map((sub) => (
 		<div key={sub}>
 			{categories[sub].map((cat) =>
-				<Link to={`/${cat.toLowerCase()}`} key={cat}>{cat}</Link>
+				<Link to={`/${cat.key.toLowerCase()}`} key={cat.key}>
+					<img src={`${WOWHEAD_ICON}${cat.icon}.jpg`}/>
+					<span>{cat.name}</span>
+				</Link>
 			)}
 		</div>
 	));
