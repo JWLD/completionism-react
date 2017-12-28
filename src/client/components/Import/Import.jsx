a04a03019412fcab0ee8e230b5009c570db5739b
JWLD <jackwldavies@icloud.com>
2017-12-28 13:22:13 +0000

    create basic Import component

---
+++ src/client/components/Import/Import.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Import.scss';
+
+const Import = () => {
+	return (
+		<div className="import-page">
+			<h1>Import</h1>
+		</div>
+	);
+};
+
+export default Import;
