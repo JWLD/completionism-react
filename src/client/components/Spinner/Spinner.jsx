06ed0b751ac5af4e8bf3931b32496c213d946a1a
JWLD <jackwldavies@icloud.com>
2017-12-28 22:26:29 +0000

    add symbol to indicate loading to user

---
+++ src/client/components/Spinner/Spinner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Spinner.scss';
+
+const Spinner = (props) => {
+	const style = {
+		height: `${props.size}rem`,
+		width: `${props.size}rem`,
+		borderWidth: `${props.size * .15}rem`
+	};
+
+	return (
+		<div className="spinner" style={style}></div>
+	);
+};
+
+export default Spinner;
