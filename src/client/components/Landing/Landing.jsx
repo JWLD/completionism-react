66ff98eaeedf0a82b1b90000f94549d3d43795bb
JWLD <jackwldavies@icloud.com>
2017-12-28 13:07:10 +0000

    reorganise css

--- src/client/components/Landing/Landing.jsx
+++ src/client/components/Landing/Landing.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import categories from 'constants/categories';
-import { WOWHEAD_ICON } from 'constants/urls';
+import { ICON_URL } from 'constants/urls';
 import './Landing.scss';
 
 const Landing = () => {
 	const wraps = Object.keys(categories).map((sub) => (
 		<div key={sub}>
 			{categories[sub].map((cat) =>
-				<Link to={`/${cat.key.toLowerCase()}`} key={cat.key}>
-					<img src={`${WOWHEAD_ICON}${cat.icon}.jpg`}/>
+				<Link className="cat-link" to={`/${cat.key.toLowerCase()}`} key={cat.key}>
+					<img src={`${ICON_URL}${cat.icon}.jpg`}/>
 					<span>{cat.name}</span>
 				</Link>
 			)}
