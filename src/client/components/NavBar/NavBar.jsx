258d6e93d6aa56154450b223ba7fb485c195140e
JWLD <jackwldavies@icloud.com>
2017-12-30 12:48:35 +0000

    add home button to nav bar

--- src/client/components/NavBar/NavBar.jsx
+++ src/client/components/NavBar/NavBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import FaHome from 'react-icons/lib/fa/home';
 
 import './NavBar.scss';
 
 const NavBar = () => (
 	<nav className="main-nav">
-
+		<Link to="/">
+			<FaHome />
+		</Link>
 	</nav>
 );
 
