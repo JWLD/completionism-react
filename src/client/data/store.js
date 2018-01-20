1a0f6a9755cfc1aaca48475adbe1b6b3ccc311a4
JWLD <jackwldavies@icloud.com>
2018-01-20 17:36:35 +0000

    add redux form and hook up to root reducer

--- src/client/data/store.js
+++ src/client/data/store.js
@@ -1,14 +1,16 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 import { reducer as browseReducer } from 'scenes/Browse/data/reducer';
 
-const appReducer = combineReducers({
+const rootReducer = combineReducers({
 	browse: browseReducer,
+	form: formReducer
 });
 
 const store = createStore(
-	appReducer,
+	rootReducer,
 	compose(
 		applyMiddleware(thunk),
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
