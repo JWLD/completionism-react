7153800118c0d11658500910789182a421785d2f
JWLD <jackwldavies@icloud.com>
2018-01-21 17:51:04 +0000

    replace ImportForm styling with styled-components

--- src/client/scenes/Import/components/ImportForm/ImportForm.jsx
+++ src/client/scenes/Import/components/ImportForm/ImportForm.jsx
@@ -3,12 +3,25 @@ import { reduxForm } from 'redux-form';
 
 import CategoryFields from './components/CategoryFields/CategoryFields';
 import CharacterFields from './components/CharacterFields/CharacterFields';
-import './ImportForm.scss';
+import * as SC from './styled';
 
 const ImportForm = () => (
 	<form className="import-form">
-		<CategoryFields />
-		<CharacterFields />
+		<SC.FormSection>
+			<SC.FormHeader>Select Categories</SC.FormHeader>
+
+			<SC.InputWrap className="sctn__input-wrap">
+				<CategoryFields />
+			</SC.InputWrap>
+		</SC.FormSection>
+
+		<SC.FormSection>
+			<SC.FormHeader>Select Character</SC.FormHeader>
+
+			<SC.InputWrap className="sctn__input-wrap">
+				<CharacterFields />
+			</SC.InputWrap>
+		</SC.FormSection>
 	</form>
 );
 
---
+++ src/client/scenes/Import/components/ImportForm/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const FormSection = styled.section`
+	:not(:last-child) {
+		margin-bottom: 3rem;
+	}
+`;
+
+export const FormHeader = styled.h2`
+	margin-bottom: 1rem;
+	color: white;
+	font: 4rem DinRC;
+`;
+
+export const InputWrap = styled.div`
+	align-items: stretch;
+	width: 70rem;
+	padding: 2rem;
+	border-radius: .4rem;
+	background-color: #333;
+`;
