79e0411bc7e66ce737f9ac95bba628339c0d0305
JWLD <jackwldavies@icloud.com>
2018-01-23 01:17:49 +0000

    add placeholder option to SelectBoxField

--- src/client/components/ReduxFields/SelectBoxField.jsx
+++ src/client/components/ReduxFields/SelectBoxField.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SelectBoxField = ({ input, options, StyledComponent }) => (
-	<StyledComponent {...input}>
-		{Object.keys(options).map(key => (
-			<option key={key} value={key}>
-				{options[key]}
-			</option>
-		))}
-	</StyledComponent>
-);
+const SelectBoxField = ({ input, options, placeholder, StyledComponent }) => {
+	const placeholderOption = placeholder
+		? <option value="">{placeholder}</option>
+		: null;
+
+	return (
+		<StyledComponent {...input}>
+			{placeholderOption}
+			{Object.keys(options).map(key => (
+				<option key={key} value={key}>
+					{options[key]}
+				</option>
+			))}
+		</StyledComponent>
+	);
+};
 
 SelectBoxField.propTypes = {
 	input: PropTypes.object.isRequired,
 	options: PropTypes.object.isRequired,
+	placeholder: PropTypes.string,
 	StyledComponent: PropTypes.func.isRequired
+};
+
+SelectBoxField.defaultProps = {
+	placeholder: null
 };
 
 export default SelectBoxField;
