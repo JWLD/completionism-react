86c9ea640f1900a2c6ba970f7f1954eca4f7538e
JWLD <jackwldavies@icloud.com>
2018-02-12 00:25:02 -0800

    remove sass from project

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,31 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-	entry: './src/client/App.jsx',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	devServer: {
-		contentBase: './public',
-		historyApiFallback: true,
-		proxy: {
-			'/api': {
-				target: 'http://localhost:8070'
-			}
-		}
-	},
-	devtool: 'cheap-eval-source-map',
-	resolve: {
-		extensions: ['.js', '.jsx', '.json'],
-		modules: [
-			path.resolve('./src/client'),
-			path.resolve('./node_modules')
-		]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				use: 'babel-loader'
-			},
-			{
-				test: /\.scss$/,
-				use: ['style-loader', 'css-loader', {
-					loader: 'sass-loader',
-					options: {
-						includePaths: [path.join(__dirname, 'public', 'sass')]
-					}
-				}]
-			}
-		]
-	}
-};
+  entry: './src/client/App.jsx',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8070'
+      }
+    }
+  },
+  devtool: 'cheap-eval-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    modules: [path.resolve('./src/client'), path.resolve('./node_modules')]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
