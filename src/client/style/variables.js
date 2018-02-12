c0dd8a320c3213f274c936a1aa5c66df3e14e4e1
JWLD <jackwldavies@icloud.com>
2018-02-12 00:23:26 -0800

    move variables into styled-components file

--- src/client/style/variables.js
+++ src/client/style/variables.js
@@ -1,20 +1,23 @@
-const pos = '#00cc66';
-const neg = '#ff6347';
-
 const colours = {
-	pos,
-	neg
-};
+  pos: '#00cc66',
+  neg: '#ff6347',
 
-const navBarHeight = '7.5';
-const pagePadding = '5';
+  q0: '#9d9d9d',
+  q1: '#ffffff',
+  q2: '#a5ff99',
+  q3: '#4980f8',
+  q4: '#bf72f3',
+  q5: '#ffa64d',
+  q6: '#99ebff',
+  q7: 'ffcc00'
+}
 
 const sizing = {
-	navBarHeight,
-	pagePadding
-};
+  navBarHeight: 7.5,
+  pagePadding: 5
+}
 
 export default {
-	colours,
-	sizing
-};
+  colours,
+  sizing
+}
