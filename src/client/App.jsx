0ad29c2252248d24d891c21d5cbd14a76b25c84a
JWLD <jackwldavies@icloud.com>
2018-02-12 00:05:17 -0800

    replace eslint with prettier and format App.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import store from './store';
+import store from './store'
 
-import Landing from './scenes/Landing';
-import Browse from './scenes/Browse';
-import Import from './scenes/Import';
+import Landing from './scenes/Landing'
+import Browse from './scenes/Browse'
+import Import from './scenes/Import'
 
 const App = () => (
-	<BrowserRouter>
-		<Provider store={store}>
-			<div className="app">
-				<Route exact path="/" component={Landing} />
-				<Route path="/browse/:category/:content" component={Browse} />
-				<Route path="/import" component={Import} />
-			</div>
-		</Provider>
-	</BrowserRouter>
-);
+  <BrowserRouter>
+    <Provider store={store}>
+      <div className="app">
+        <Route exact path="/" component={Landing} />
+        <Route path="/browse/:category/:content" component={Browse} />
+        <Route path="/import" component={Import} />
+      </div>
+    </Provider>
+  </BrowserRouter>
+)
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'))
