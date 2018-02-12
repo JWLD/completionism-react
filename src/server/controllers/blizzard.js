d08088f2f5df48c47f8c813294b03bcd538d4d3d
JWLD <jackwldavies@icloud.com>
2018-02-11 23:18:13 -0800

    move ranked data checking onto backend

--- src/server/controllers/blizzard.js
+++ src/server/controllers/blizzard.js
@@ -1,10 +1,15 @@
 const request = require('request');
+const async = require('async');
 
 const { PRIMARY, SECONDARY } = require('../constants/categories');
+
+const dbConnect = require('../dbConnect');
+const dbQueries = require('../dbQueries');
 
 const blizzController = {};
 
 // GET REALMS - GET REALM LIST FROM BLIZZARD API
+
 const constructRealmDataUrl = params =>
 	`https://${params.q}.api.battle.net/wow/realm/status
 	?locale=en_GB&apikey=${process.env.BATTLENET_KEY}`;
@@ -33,6 +38,7 @@ blizzController.getRealmData = (req, res) => {
 };
 
 // GET IMPORT - GET CHAR DATA FROM BLIZZARD API
+
 const constructFieldsArray = (cats) => {
 	const catsArray = typeof cats === 'string' ? [cats] : cats;
 
@@ -79,26 +85,80 @@ const extractPetCollectionData = blizzData =>
 		quality: pet.qualityId
 	}));
 
-const extractProfessionData = (collectionData, blizzData, cat) => {
-	const data = Object.assign({}, collectionData);
+const processRankedDataFromDB = (data) => {
+	const rankedData = {
+		one: data.filter(item => item.rank === 1),
+		two: data.filter(item => item.rank === 2),
+		three: data.filter(item => item.rank === 3)
+	};
 
+	return rankedData;
+};
+
+const findMissingIds = (recipeIds, rankedData) => {
+	const missingIds = [];
+
+	recipeIds.forEach((recipeId) => {
+		const rank3Recipe = rankedData.three.find(item => item.id === recipeId);
+		const rank2Recipe = rankedData.two.find(item => item.id === recipeId);
+
+		if (rank3Recipe) {
+			const rank1Id = rankedData.one.find(item => item.name === rank3Recipe.name).id;
+			const rank2Id = rankedData.two.find(item => item.name === rank3Recipe.name).id;
+
+			missingIds.push(rank1Id, rank2Id);
+		} else if (rank2Recipe) {
+			const rank1Id = rankedData.one.find(item => item.name === rank2Recipe.name).id;
+
+			missingIds.push(rank1Id);
+		}
+	});
+
+	return missingIds;
+};
+
+const checkForRankedRecipes = (recipeIds, cat, blizzData, callback) => {
+	dbQueries.getRankedData(dbConnect, cat, (err, res) => {
+		if (err) return console.log(err);
+
+		const rankedData = processRankedDataFromDB(res);
+		const missingIds = findMissingIds(recipeIds, rankedData);
+
+		const completeIds = recipeIds.concat(missingIds);
+
+		const data = {
+			name: cat,
+			ids: completeIds
+		};
+
+		return callback(null, data);
+	});
+};
+
+const extractProfessionData = (blizzData, cat, callback) => {
 	const profType = PRIMARY.includes(cat) ? 'primary' : 'secondary';
 	const profIndex = blizzData.professions[profType].findIndex(prof =>
-		prof.name.toLowerCase().split(' ').join('') === cat);
+		prof.name
+			.toLowerCase()
+			.split(' ')
+			.join('') === cat);
 	const charHasThisProfession = profIndex !== -1;
 
 	if (charHasThisProfession) {
-		data.collections[cat] = blizzData.professions[profType][profIndex].recipes;
-	}
+		const recipeIds = blizzData.professions[profType][profIndex].recipes;
 
-	return data;
+		checkForRankedRecipes(recipeIds, cat, blizzData, callback);
+	}
 };
 
-const extractCollectionData = (cats, charData, blizzData) => {
-	let data = Object.assign({}, charData);
+const extractCollectionData = (cats, charData, blizzData, res) => {
+	const data = Object.assign({}, charData);
+
 	data.collections = {};
 
 	const catsArray = typeof cats === 'string' ? [cats] : cats;
+
+	const tasks = [];
 
 	catsArray.forEach((cat) => {
 		const catIsPrimaryProfession = PRIMARY.includes(cat);
@@ -108,14 +168,22 @@ const extractCollectionData = (cats, charData, blizzData) => {
 			data.collections.mounts = extractMountCollectionData(blizzData);
 		} else if (cat === 'pets') {
 			data.collections.pets = extractPetCollectionData(blizzData);
-		}	else if (catIsPrimaryProfession) {
-			data = extractProfessionData(data, blizzData, cat);
+		} else if (catIsPrimaryProfession) {
+			tasks.push(callback => extractProfessionData(blizzData, cat, callback));
 		} else if (catIsSecondaryProfession) {
-			data = extractProfessionData(data, blizzData, cat);
+			tasks.push(callback => extractProfessionData(blizzData, cat, callback));
 		}
 	});
 
-	return data;
+	async.parallel(tasks, (err, results) => {
+		if (err) return console.log(err);
+
+		results.forEach((result) => {
+			data.collections[result.name] = result.ids;
+		});
+
+		return res.send(data);
+	});
 };
 
 blizzController.getCharData = (req, res) => {
@@ -126,12 +194,10 @@ blizzController.getCharData = (req, res) => {
 
 		const blizzData = JSON.parse(body);
 
-		if (blizzData.status === 'nok') return res.status(500).send(blizzData.reason);
+		if (blizzData.status === 'nok') { return res.status(500).send(blizzData.reason); }
 
-		let charData = constructCharDataObject(blizzData, req.query.region);
-		charData = extractCollectionData(req.query.cats, charData, blizzData);
-
-		return res.send(charData);
+		const charData = constructCharDataObject(blizzData, req.query.region);
+		return extractCollectionData(req.query.cats, charData, blizzData, res);
 	});
 };
 
