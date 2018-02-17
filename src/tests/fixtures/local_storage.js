469e1e767b8792b2f186644b93db83b0dc3ff5dc
JWLD <jackwldavies@icloud.com>
2018-02-16 20:08:57 -0800

    add test fixtures folder

---
+++ src/tests/fixtures/local_storage.js
@@ -0,0 +1,9 @@
+export const mockMounts = JSON.stringify({
+  char: {
+    class: 3,
+    faction: 0,
+    region: 'eu',
+    thumb: 'sylvanas/92/99597404-avatar.jpg'
+  },
+  ids: [2538, 2539, 2540, 2541, 2542]
+})
