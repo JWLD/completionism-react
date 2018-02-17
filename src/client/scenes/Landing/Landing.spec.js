ce5e8bf0ee3d2c1d7e01cc75809fe39cdd5210c1
JWLD <jackwldavies@icloud.com>
2018-02-16 20:09:42 -0800

    update Landing.spec.js to use new localStorage fixtures

--- src/client/scenes/Landing/Landing.spec.js
+++ src/client/scenes/Landing/Landing.spec.js
@@ -3,8 +3,11 @@ import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 import Landing from 'scenes/Landing'
+import { mockMounts } from 'fixtures/local_storage'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+localStorage.mounts = mockMounts
 
 describe('<Landing />', () => {
   test('renders correctly', () => {
