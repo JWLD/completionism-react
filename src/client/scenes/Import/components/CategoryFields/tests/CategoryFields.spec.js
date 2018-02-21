0d581e16ba950908b97cc5e735aca62a095b6f3c
JWLD <jackwldavies@icloud.com>
2018-02-20 23:32:56 -0800

    add snapshot test for CategoryFields

---
+++ src/client/scenes/Import/components/CategoryFields/tests/CategoryFields.spec.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import CategoryFields from 'scenes/Import/components/CategoryFields'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<CategoryFields />', () => {
+  test('renders correctly', () => {
+    const component = shallow(<CategoryFields />)
+
+    expect(component).toMatchSnapshot()
+  })
+})
