fe414c6c24d2011fe318824a864a7d386727738c
JWLD <jackwldavies@icloud.com>
2018-02-20 23:40:15 -0800

    run import redux files through prettier

--- src/client/scenes/Import/actions.js
+++ src/client/scenes/Import/actions.js
@@ -1,15 +1,18 @@
-import axios from 'axios';
+import axios from 'axios'
 
-export const ADD_REALM_DATA = 'ADD_REALM_DATA';
+export const ADD_REALM_DATA = 'ADD_REALM_DATA'
 
 export const getRealmData = (realmData, region) => ({
-	type: ADD_REALM_DATA,
-	payload: realmData,
-	region
-});
+  type: ADD_REALM_DATA,
+  payload: realmData,
+  region
+})
 
-export const fetchRealmData = region => (dispatch) => {
-	axios.get(`/api/realms?q=${region}`).then((response) => {
-		dispatch(getRealmData(response.data, region));
-	}).catch(err => console.log(err));
-};
+export const fetchRealmData = region => dispatch => {
+  axios
+    .get(`/api/realms?q=${region}`)
+    .then(response => {
+      dispatch(getRealmData(response.data, region))
+    })
+    .catch(err => console.log(err))
+}
--- src/client/scenes/Import/reducer.js
+++ src/client/scenes/Import/reducer.js
@@ -1,16 +1,19 @@
-import { ADD_REALM_DATA } from './actions';
+import { ADD_REALM_DATA } from './actions'
 
 const initialState = {
-	realms: {}
-};
+  realms: {}
+}
 
 const importReducer = (state = initialState, action) => {
-	switch (action.type) {
-	case ADD_REALM_DATA:
-		return { ...state, realms: { ...state.realms, [action.region]: action.payload } };
-	default:
-		return state;
-	}
-};
+  switch (action.type) {
+    case ADD_REALM_DATA:
+      return {
+        ...state,
+        realms: { ...state.realms, [action.region]: action.payload }
+      }
+    default:
+      return state
+  }
+}
 
-export default importReducer;
+export default importReducer
