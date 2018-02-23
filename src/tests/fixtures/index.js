2ac6a064858e9cd9c1bf1acd99239584ff11c902
JWLD <jackwldavies@icloud.com>
2018-02-22 23:25:14 -0800

    add redux state fixtures

--- src/tests/fixtures/index.js
+++ src/tests/fixtures/index.js
@@ -1,3 +1,4 @@
 export * from './api'
 export * from './form_values'
 export * from './local_storage'
+export * from './redux_state'
---
+++ src/tests/fixtures/redux_state.js
@@ -0,0 +1,7 @@
+export const MOCK_INITIAL_STATE = {
+  browse: {},
+  form: {},
+  import: {
+    realms: {}
+  }
+}
