cd295790567dc03dcf732a86bab6afbea80464d5
JWLD <jackwldavies@icloud.com>
2018-02-22 20:06:17 -0800

    add snapshot tests for form field components

---
+++ src/client/components/formFields/CheckboxField/CheckboxField.spec.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import { CheckboxField } from 'components/formFields'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+const mockProps = {
+  input: {},
+  StyledComponent: jest.fn()
+}
+
+describe('<CheckboxField />', () => {
+  test('renders correctly', () => {
+    const component = shallow(<CheckboxField {...mockProps} />)
+
+    expect(component).toMatchSnapshot()
+  })
+})
---
+++ src/client/components/formFields/SelectBoxField/SelectBoxField.spec.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import { SelectBoxField } from 'components/formFields'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+const mockProps = {
+  input: {},
+  options: {
+    foo: 'bar'
+  },
+  placeholder: 'Fantabulous',
+  StyledComponent: jest.fn()
+}
+
+describe('<SelectBoxField />', () => {
+  test('renders correctly', () => {
+    const component = shallow(<SelectBoxField {...mockProps} />)
+
+    expect(component).toMatchSnapshot()
+  })
+})
