8174dd4a021b6f57a2a6f08cf19e9023cb39c89e
JWLD <jackwldavies@icloud.com>
2018-02-23 20:32:57 -0800

    unwrap promises around api calls

--- src/client/services/api.js
+++ src/client/services/api.js
@@ -3,29 +3,25 @@ import axios from 'axios'
 import * as URLS from 'constants/api_urls'
 
 export const fetchBattleNetCharacterData = params => {
-  return new Promise(resolve =>
-    axios
-      .get(URLS.IMPORT_URL, {
-        params
-      })
-      .then(res => resolve(res.data))
-      .catch(err =>
-        console.log(`Error fetching character data from BattleNet: ${err}`)
-      )
-  )
+  return axios
+    .get(URLS.IMPORT_URL, { params })
+    .then(res => {
+      console.log('Success fetching character data from BattleNet!')
+      return res.data
+    })
+    .catch(err =>
+      console.log(`Error fetching character data from BattleNet: ${err}`)
+    )
 }
 
 export const fetchBattleNetRealmData = region => {
-  return new Promise(resolve => {
-    axios
-      .get(URLS.REALM_URL, {
-        params: {
-          region
-        }
-      })
-      .then(res => resolve(res.data))
-      .catch(err =>
-        console.log(`Error fetching realm data from BattleNet: ${err}`)
-      )
-  })
+  return axios
+    .get(URLS.REALM_URL, { params: { region } })
+    .then(res => {
+      console.log('Success fetching realm data from BattleNet!')
+      return res.data
+    })
+    .catch(err =>
+      console.log(`Error fetching realm data from BattleNet: ${err}`)
+    )
 }
