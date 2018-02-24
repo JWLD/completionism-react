9456448deb2a9055b994083a31e3bcde83c1a230
JWLD <jackwldavies@icloud.com>
2018-02-23 20:37:09 -0800

    make use of url constants in utils test

--- src/client/scenes/Import/components/ImportForm/tests/utils.spec.js
+++ src/client/scenes/Import/components/ImportForm/tests/utils.spec.js
@@ -3,12 +3,13 @@ import MockAdapter from 'axios-mock-adapter'
 
 import { fetchCharData } from 'scenes/Import/components/ImportForm/utils'
 import * as mocks from 'fixtures'
+import { IMPORT_URL } from 'constants/api_urls'
 
 const mock = new MockAdapter(axios)
 
 describe('#fetchCharData', () => {
   it('fetches data from BattleNet and saves it to localStorage', () => {
-    mock.onGet('/api/import').reply(200, mocks.battleNetCharacterData)
+    mock.onGet(IMPORT_URL).reply(200, mocks.battleNetCharacterData)
 
     fetchCharData(mocks.importFormValues)
 
