22f099e847881965ed4a0ff21a8916221498a527
JWLD <jackwldavies@icloud.com>
2018-02-25 19:01:38 -0800

    add componentDidMount tests for Browse

--- src/client/scenes/Browse/tests/Browse.spec.js
+++ src/client/scenes/Browse/tests/Browse.spec.js
@@ -6,16 +6,38 @@ import { Browse } from 'scenes/Browse'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-const mockProps = {
-  category: '',
-  categoryData: [],
-  fetchCategoryData: jest.fn()
-}
+const mockFetchCategoryData = jest.fn()
+let mockProps = {}
+
+beforeEach(() => {
+  mockProps = {
+    category: '',
+    categoryDataExists: false,
+    fetchCategoryData: mockFetchCategoryData
+  }
+
+  jest.resetAllMocks()
+})
 
 describe('<Browse />', () => {
   it('renders correctly', () => {
     const component = shallow(<Browse {...mockProps} />)
 
     expect(component).toMatchSnapshot()
+  })
+})
+
+describe('#componentDidMount', () => {
+  it('calls [fetchCategoryData] is [categoryDataExists] is false', () => {
+    shallow(<Browse {...mockProps} />)
+
+    expect(mockFetchCategoryData).toHaveBeenCalledTimes(1)
+  })
+
+  it('does not call [fetchCategoryData] when categoryData is populated', () => {
+    mockProps.categoryDataExists = true
+    shallow(<Browse {...mockProps} />)
+
+    expect(mockFetchCategoryData).toHaveBeenCalledTimes(0)
   })
 })
