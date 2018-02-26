3737ab346edf9c5b133879fa0d7249ed066d4cb4
JWLD <jackwldavies@icloud.com>
2018-02-25 20:06:43 -0800

    add snapshot test for DetailPanel

---
+++ src/client/scenes/Browse/components/DetailPanel/tests/DetailPanel.spec.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import DetailPanel from 'scenes/Browse/components/DetailPanel'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<DetailPanel />', () => {
+  it('renders correctly', () => {
+    const component = shallow(<DetailPanel />)
+
+    expect(component).toMatchSnapshot()
+  })
+})
