6e1d6a544fbbcb4cc5944a0a650c90c6e4bba6a4
JWLD <jackwldavies@icloud.com>
2018-02-25 19:09:03 -0800

    adjust selector code layout

--- src/client/scenes/Browse/selectors.js
+++ src/client/scenes/Browse/selectors.js
@@ -7,5 +7,7 @@ const browseStateSelector = state => state.browse
 
 export const categoryDataExistsSelector = createSelector(
   [browseStateSelector, categoryParamSelector],
-  (browseState, categoryParam) => (browseState[categoryParam] ? true : false)
+  (browseState, categoryParam) => {
+    return browseState[categoryParam] ? true : false
+  }
 )
