37781fc64a4285b5ba8922907215f21bbda2dbb9
JWLD <jackwldavies@icloud.com>
2018-04-08 17:18:53 -0700

    run prettier on blizzard constants file

--- src/client/constants/blizzard.js
+++ src/client/constants/blizzard.js
@@ -1,31 +1,31 @@
 export const CLASSES = {
-	1: 'Warrior',
-	2: 'Paladin',
-	3: 'Hunter',
-	4: 'Rogue',
-	5: 'Priest',
-	6: 'Death Knight',
-	7: 'Shaman',
-	8: 'Mage',
-	9: 'Warlock',
-	10: 'Monk',
-	11: 'Druid',
-	12: 'Demon Hunter'
-};
+  1: 'Warrior',
+  2: 'Paladin',
+  3: 'Hunter',
+  4: 'Rogue',
+  5: 'Priest',
+  6: 'Death Knight',
+  7: 'Shaman',
+  8: 'Mage',
+  9: 'Warlock',
+  10: 'Monk',
+  11: 'Druid',
+  12: 'Demon Hunter'
+}
 
 export const FACTIONS = {
-	0: 'Alliance',
-	1: 'Horde',
-	2: 'Neutral'
-};
+  0: 'Alliance',
+  1: 'Horde',
+  2: 'Neutral'
+}
 
 export const REGIONS = {
-	eu: 'EU',
-	us: 'US'
-};
+  eu: 'EU',
+  us: 'US'
+}
 
 export default {
-	CLASSES,
-	FACTIONS,
-	REGIONS
-};
+  CLASSES,
+  FACTIONS,
+  REGIONS
+}
