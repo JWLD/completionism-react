dafc58373f680a43248c49023af3bd87ec93883c
JWLD <jackwldavies@icloud.com>
2018-05-01 21:06:58 -0700

    change how pet data is saved in local storage

--- src/client/scenes/Import/components/ImportForm/utils.js
+++ src/client/scenes/Import/components/ImportForm/utils.js
@@ -19,12 +19,25 @@ const constructUrlParams = ({ categories, character }) => ({
   region: character.region
 })
 
+const transformPetDataForStorage = petData => {
+  return petData.reduce((acc, pet) => {
+    acc[pet.id] = {
+      level: pet.level,
+      quality: pet.quality
+    }
+
+    return acc
+  }, {})
+}
+
 const saveCharDataToLocalStorage = ({ char, collections }) => {
   return Object.keys(collections).map(key => {
-    localStorage[key] = JSON.stringify({
-      char,
-      ids: collections[key]
-    })
+    const collection =
+      key === 'pets'
+        ? transformPetDataForStorage(collections[key])
+        : collections[key]
+
+    localStorage[key] = JSON.stringify({ char, collection })
   })
 }
 
