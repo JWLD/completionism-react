20fc694071b1a68a2fa77db166a854afaac80995
JWLD <jackwldavies@icloud.com>
2018-05-07 21:48:44 -0600

    write selector to get progress data for each category on Landing page

--- src/client/scenes/Landing/components/CategoryPanel/index.jsx
+++ src/client/scenes/Landing/components/CategoryPanel/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import { ICON_URLS } from 'constants/urls'
 import { getCharacterDataFromLocalStorage } from 'services/local_storage'
 
+import { getProgressData } from 'CategoryPanel/selectors'
 import * as SC from 'CategoryPanel/styled'
 
-const CategoryPanel = ({ category, icon, name }) => {
-  const renderPortrait = characterData => {
-    const { region, thumb } = characterData
+const CategoryPanel = ({ category, icon, name, progress }) => {
+  console.log(progress)
+
+  // eslint-disable-next-line
+  const renderPortrait = ({ region, thumb }) => {
     const portraitUrl = `http://render-${region}.worldofwarcraft.com/character/${thumb}`
 
     return <SC.Portrait src={portraitUrl} />
@@ -36,9 +40,14 @@ const CategoryPanel = ({ category, icon, name }) => {
 }
 
 CategoryPanel.propTypes = {
-  category: PropTypes.string,
-  icon: PropTypes.string,
-  name: PropTypes.string
+  category: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  progress: PropTypes.object.isRequired
 }
 
-export default CategoryPanel
+const mapStateToProps = (state, ownProps) => ({
+  progress: getProgressData(state, ownProps)
+})
+
+export default connect(mapStateToProps)(CategoryPanel)
