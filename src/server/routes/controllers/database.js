9ac4a03203a6bcb8188b3eb8a91ff1033434c390
JWLD <jackwldavies@icloud.com>
2018-06-02 11:12:01 +0100

    modify back-end to send all category data in single request

--- src/server/routes/controllers/database.js
+++ src/server/routes/controllers/database.js
@@ -1,14 +1,36 @@
+const parallel = require('async/parallel')
+
 const { getCategoryData, getRankedData } = require('../../database/queries')
+const CATEGORIES = require('../../constants/cats')
 
 const dbController = {}
 
-// GET DB-CATEGORY - GET DATA FOR A SPECIFIC CATEGORY FROM DB
+// GET DB-CATEGORY - GET ALL CATEGORY DATA FROM DB
 
 dbController.getCategoryData = (req, res) => {
-  getCategoryData(req.query.category, (err, dbRes) => {
+  const tasks = CATEGORIES.map(category => {
+    return callback => {
+      return getCategoryData(category, (err, dbRes) => {
+        if (err) return callback(err)
+
+        const foo = {
+          category,
+          data: dbRes
+        }
+
+        return callback(null, foo)
+      })
+    }
+  })
+
+  parallel(tasks, (err, results) => {
     if (err) return res.status(500).send(err)
 
-    return res.send(dbRes)
+    const dataObject = results.reduce((acc, result) => {
+      return { ...acc, [result.category]: result.data }
+    }, {})
+
+    return res.send(dataObject)
   })
 }
 
