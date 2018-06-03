4283aba4c83adecb4e2c88a2d1ac7b3c9517bdb1
JWLD <jackwldavies@icloud.com>
2018-06-03 19:33:11 +0100

    add title to landing page sections

--- src/client/scenes/Landing/index.jsx
+++ src/client/scenes/Landing/index.jsx
@@ -4,7 +4,7 @@ import { CATEGORIES_BY_SECTION as CATEGORIES } from 'constants/categories'
 import { PageWrap } from 'style/components'
 import NavBar from 'NavBar'
 
-import * as SC from 'Landing/styled'
+import { SectionTitle, SectionWrap } from 'Landing/styled'
 import CategoryPanel from 'CategoryPanel'
 
 const Landing = () => {
@@ -20,9 +20,10 @@ const Landing = () => {
   }
 
   const categoryBlocks = CATEGORIES.map(section => (
-    <SC.CategoryBlock key={section.key}>
+    <SectionWrap key={section.key}>
+      <SectionTitle>{section.key}</SectionTitle>
       {renderCategoryPanels(section)}
-    </SC.CategoryBlock>
+    </SectionWrap>
   ))
 
   return (
--- src/client/scenes/Landing/styled.js
+++ src/client/scenes/Landing/styled.js
@@ -1,8 +1,19 @@
 import styled from 'styled-components'
 
+import { mixins } from 'style'
+
 const panelPadding = 1
 
-export const CategoryBlock = styled.ul`
+export const SectionTitle = styled.span`
+  margin-bottom: 0.5rem;
+  color: white;
+  font: 3.5rem DinMC;
+  text-transform: uppercase;
+`
+
+export const SectionWrap = styled.ul`
+  ${mixins.flexVerticalLeft};
+
   :not(:last-child) {
     margin-bottom: ${panelPadding * 3}rem;
   }
