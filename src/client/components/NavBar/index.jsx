8bf382e4a48055531f685d8199e5539878172da8
JWLD <jackwldavies@icloud.com>
2018-07-28 19:27:40 +0100

    simplify nav bar and add link to import page

--- src/client/components/NavBar/index.jsx
+++ src/client/components/NavBar/index.jsx
@@ -1,39 +1,11 @@
-import React from 'react'
-import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import React from 'react'
 import { withRouter } from 'react-router-dom'
 
 import * as SC from 'NavBar/styled'
 
-import { CONTENT } from 'constants/content'
-import { changeBrowsePage } from 'Browse/actions'
-
-export const NavBar = ({ changeBrowsePage, match }) => {
-  const renderCentreNav = () => {
-    if (!match.params.category) return null
-
-    const { content } = match.params
-
-    return (
-      <SC.CentreWrap>
-        <SC.NavButton onClick={() => changeBrowsePage({ next: false })}>
-          <SC.CircleLeftIcon />
-        </SC.NavButton>
-
-        <SC.MainTitle>{CONTENT[content].content}</SC.MainTitle>
-
-        <SC.NavButton onClick={() => changeBrowsePage({ next: true })}>
-          <SC.CircleRightIcon />
-        </SC.NavButton>
-      </SC.CentreWrap>
-    )
-  }
-
-  const renderCategorySpan = () => {
-    const { category } = match.params
-
-    return category && <SC.NavSpan>{category}</SC.NavSpan>
-  }
+export const NavBar = ({ match }) => {
+  const { category } = match.params
 
   return (
     <SC.NavBar>
@@ -42,23 +14,18 @@ export const NavBar = ({ changeBrowsePage, match }) => {
           <SC.HomeIcon />
         </SC.NavLink>
 
-        {renderCategorySpan()}
+        {category && <SC.NavSpan>{category}</SC.NavSpan>}
       </SC.LeftWrap>
 
-      {renderCentreNav()}
+      <SC.NavLink to="/import">
+        <SC.CharacterPageLink />
+      </SC.NavLink>
     </SC.NavBar>
   )
 }
 
 NavBar.propTypes = {
-  changeBrowsePage: PropTypes.func.isRequired,
   match: PropTypes.object.isRequired
 }
 
-const mapDispatchToProps = {
-  changeBrowsePage
-}
-
-const ReduxComponent = connect(null, mapDispatchToProps)(NavBar)
-
-export default withRouter(ReduxComponent)
+export default withRouter(NavBar)
