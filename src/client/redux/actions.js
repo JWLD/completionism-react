3e1612b3cf0e68e367284bae912dfca7ba8e8a9b
JWLD <jackwldavies@icloud.com>
2018-08-23 20:20:28 +0100

    use new api functions throughout app

--- src/client/redux/actions.js
+++ src/client/redux/actions.js
@@ -1,17 +1,13 @@
 import * as ACTIONS from 'constants/action_types'
-import api from 'services/api'
+import { fetchAllCategoryData } from 'services/api'
 
 export const setCategoryData = data => ({
   type: ACTIONS.SET_CATEGORY_DATA,
   data
 })
 
-export const fetchCategoryData = () => dispatch => {
-  return api
-    .getCategoryData()
-    .then(data => {
-      console.log(data)
-      dispatch(setCategoryData(data))
-    })
-    .catch(err => console.log(err))
+export const fetchCategoryData = () => async dispatch => {
+  const categoryData = await fetchAllCategoryData()
+
+  dispatch(setCategoryData(categoryData))
 }
